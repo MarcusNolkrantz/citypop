275264f7a012d73b01e7a5fe007ed3ae17149578
Marcus Nolkrantz <marno874@student.liu.se>
2019-10-02 14:32:59 +0200

    Added render logic and event functions for select- and show components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+//Imports
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Component imports
+import Select from "./components/select.js";
+import Search from "./components/search.js";
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			page: "select",
+			searchBy: ""
+		};
+	}
+
+
+	//////////////////////////////////////////////
+	//Event functions
+	//////////////////////////////////////////////
+
+	//Update searchBy state, country or city
+	//change page state to search
+	buttonHandler = (type) => {
+		this.setState({searchBy: type, page: "search"});
+	}
+
+	//Update searchTerm state to users input
+	//change page state to show
+	submitHandler = (searchTerm) => {
+		this.setState({searchTerm: searchTerm, page: "show"});
+	}
+
+
+	
+
+
+	render() {
+		return (
+			<div>
+				<h1>CityPop</h1>  
+				{this.state.page === "select" && <Select buttonHandler = {this.buttonHandler}/>}
+				{this.state.page === "search" && <Search submitHandler = {this.submitHandler} searchBy = {this.state.searchBy}/>}
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
