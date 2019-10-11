e994d4e595af0c884d9d5f97597272431419c8eb
Marcus Nolkrantz <marno874@student.liu.se>
2019-10-11 12:08:37 +0200

    Added suggestions if search is not a valid city- or country name

--- src/components/show.js
+++ src/components/show.js
@@ -18,8 +18,8 @@ class Show extends Component{
 			loading: false,
 			cities: null,
 			error: null,
-			cityName: "",
-			cityPopulation: 0
+			cityPopulation: 0,
+			placeName: this.props.searchTerm
 		};
 	};
 
@@ -33,14 +33,14 @@ class Show extends Component{
 		//If user wants to search for a country.
 		if(this.props.searchBy === "country"){
 			//Lookup country code from searchterm.
-			const countryCode = lookup.findCountry({'name': this.props.searchTerm}, true);
+			const countryCode = lookup.findCountry({'name': this.state.placeName});
 			//Country not found.
 			if(!countryCode){
 				//Hide loading icon and set error message.
 				this.setState({loading: false, error: "The country could not be found"});
 			}
 			else{
-				this.countryApiCall(countryCode.a2);
+				this.countryApiCall(countryCode.a2, countryCode.name);
 			}
 		}	
 		//If user wants to search for a city.
@@ -49,12 +49,23 @@ class Show extends Component{
 		}
 	}
 
+	//This function update states depending on if 
+	// the searchterm is equal to the suggested name or not.
+	validateSearch = (searchTerm, suggestion) =>{
+		//Check if the searchterm is equal to the suggested name.
+		if(searchTerm.toLowerCase() !== suggestion.toLowerCase()){
+			//Update display state.
+			this.setState({display: "confirm"});
+		}
+		//Update palceName state.
+		this.setState({placeName: suggestion});
+	}
+
 
 	//Function fetches data about the ten most populated
 	//cities in a country,
 	//sorting by population
-	countryApiCall = (countryCode) =>{
-		const country = this.props.searchTerm;
+	countryApiCall = (countryCode, country) =>{
 		const url = "http://api.geonames.org/search?type=json&q=" + country + 
 								"&country=" + countryCode + "&orderby=population&featureClass=P&maxRows=3&username=weknowit";
 		
@@ -76,11 +87,12 @@ class Show extends Component{
 				//Check if response is empty.
 				if(!data.geonames[0]){
 					//Hide loading icon and set error message.
-					this.setState({error: "The country could not be found", loading: false});
+					this.setState({error: "The country " + country + "could not be found", loading: false});
 				}
 				//If response contains data.
 				else{
 					this.mapData(data.geonames);
+					this.validateSearch(this.state.placeName, country);
 				}
 			})
 			//Catch any errors that might have been thrown.
@@ -90,7 +102,7 @@ class Show extends Component{
 
 	//Function fetches data about a city.
 	cityApiCall = () => {
-		const city = this.props.searchTerm;
+		const city = this.state.placeName;
 		const url  = "http://api.geonames.org/search?type=json&name_equals=" + city + 
 									"&cities=cities15000&featureCode=P&username=weknowit";
 		//Fetch data from api.						
@@ -105,9 +117,11 @@ class Show extends Component{
 			})
 			.then(response => response.json())
 			.then(data => {
+				
 				const foundCity = data.geonames[0];
+
 				//Check if response is empty.
-				if(!foundCity || this.props.searchTerm === ""){
+				if(!foundCity){
 					//Hide loading icon and set error message.
 					this.setState({error: "The city could not be found", loading: false});
 				}
@@ -116,10 +130,10 @@ class Show extends Component{
 					//Hide loading icon, update city- name and population, display only one city.
 					this.setState({
 						loading: false,
-						cityName: foundCity.name,
 						cityPopulation: foundCity.population,
 						display: "city"
 					});
+					this.validateSearch(this.state.placeName, foundCity.name);
 				}
 			})	
 			//Catch any errors that might have been thrown.	
@@ -152,7 +166,7 @@ class Show extends Component{
 	//after a country search.
 	handleClick = (name, population) =>{
 		//Change the diplay data to only one city, update city- name and population.
-		this.setState({display: "city", cityName: name, cityPopulation: population})
+		this.setState({display: "city", placeName: name, cityPopulation: population})
 	}
 
 
@@ -162,18 +176,21 @@ class Show extends Component{
 			const cities 					=  this.state.cities;
 			const isLoading 			=  this.state.loading;
 			const display					=  this.state.display;
-			const cityName				=  this.state.cityName;
+			const placeName				=  this.state.placeName;
 			const cityPopulation	=  <NumberFormat value={this.state.cityPopulation} 
 																displayType={'text'} thousandSeparator=" " />
 			const error   				=  this.state.error;
-			const searchTerm			=  this.props.searchTerm;
-
+			const searchBy				=  this.props.searchBy;
+			const searchTerm      =  this.props.searchTerm;
 		//Return different components depending on the constants above. 
 		return(
 			<div className="cityList">
 				{isLoading && <Loader />}
-				{!error && !isLoading && display === "country" && <div> <h2>{searchTerm}</h2> {cities} </div>}
-				{!error && !isLoading && display === "city" && <City name = {cityName} population = {cityPopulation} />}
+				{!error && !isLoading && display === "country" && <div> <h2>{placeName}</h2> {cities} </div>}
+				{!error && !isLoading && display === "city" && <City name = {placeName} population = {cityPopulation} />}
+				{!error && !isLoading && display === "confirm" && <p>{searchTerm} can not be found. Do you want to search for </p>}
+				{!error && !isLoading && display === "confirm" && <button onClick = {() => this.setState({display: searchBy})}>
+				{this.state.placeName}</button>}
 				{error && <p>{this.state.error}</p>}
 			</div>
 			
